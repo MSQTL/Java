24153552156583634d4cf765f4de4582321b52e9
Серж <93073993+MSQTL@users.noreply.github.com>
2023-02-28 14:47:40 +0300

    full work

--- Массивы.java
+++ Массивы.java
@@ -1,18 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class HomeWorkApp {
+import static java.lang.Math.abs;
+
+public class Main {
 
     static int[] array;
-    static int userNum, size;
+    static int[][] dArray;
+    static int userNum, size, minValue, maxValue, sumLeft = 0 ,sumRight = 0;
     static Scanner in = new Scanner(System.in);
     static Random rand = new Random();
 
     public static void main(String[] arg){
 
-        Ex3();
+        Ex8();
     }
     public static void Ex1(){
 
@@ -57,7 +61,7 @@ public class HomeWorkApp {
             System.out.print(array[i] + " ");
         }
     }
-    
+
     public static void Ex4(){
 
         System.out.print("Введите размер массива: ");
@@ -83,13 +87,11 @@ public class HomeWorkApp {
         array = methodForEx5(size, userNum);
         System.out.println(Arrays.toString(array));
     }
-
     public static int[] methodForEx5(int len, int initialValue){
 
         Arrays.fill(array = new int[size], initialValue);
         return array;
     }
-
     public static void Ex6(){
 
         System.out.print("Введите длину массива: ");
@@ -109,5 +111,73 @@ public class HomeWorkApp {
         }
         System.out.println("Минимальное значение: " + minValue);
         System.out.println("Максимальное значение: " + maxValue);
+    }
+    public static void Ex7(){
+
+        System.out.print("Введите длину массива: ");
+        size = in.nextInt();
+        array = new int[size];
+
+        for (int i = 0; i < size; i++){
+            array[i] = rand.nextInt(10);
+        }
+
+        System.out.println(Arrays.toString(array));
+        System.out.println("Результат: " + methodEx7(array));
+    }
+    public static boolean methodEx7(int[] array){
+
+        for (int i = 0; i < array.length - 1; i++){
+
+            for(int j = 0; j <= i; j++) sumLeft += array[j];
+            for(int k = i + 1; k < array.length; k++) sumRight += array[k];
+            if (sumLeft == sumRight) {
+                System.out.println(sumLeft + " " + sumRight);
+                return true;
+            }
+            sumRight = 0;
+            sumLeft = 0;
+        }
+        return false;
+    }
+    public static void Ex8(){
+
+        System.out.print("Введите длину массива: ");
+        size = in.nextInt();
+        array = new int[size];
+
+        for (int i = 0; i < size; i++){
+            array[i] = rand.nextInt(10);
+        }
+
+        System.out.println(Arrays.toString(array));
+        System.out.print("На сколько сдвинуть числа? ");
+        userNum = in.nextInt();
+        methodEx8(array, userNum);
+    }
+    public static void methodEx8(int[] array, int userNum){
+
+        if (userNum > 0)
+            for (int i = 0; i < userNum; i++){
+                int a = array[0];
+                array[0] = array[array.length - 1];
+                for (int j = 1; j < array.length; j++){
+                    int b = array[j];
+                    array[j] = a;
+                    a = b;
+                }
+        }
+        else
+            for (int i = 0; i < abs(userNum); i++){
+                int a = array[array.length - 1];
+                array[array.length - 1] = array[0];
+                for (int j = array.length - 2; j >= 0 ; j--){
+                    int b = array[j];
+                    array[j] = a;
+                    a = b;
+                }
+            }
+
+        System.out.println(Arrays.toString(array));
     }
 }
