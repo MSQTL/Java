742455ffdcddb6d346f4e2b49e5bd72ba4eb6aed
Серж <93073993+MSQTL@users.noreply.github.com>
2023-03-01 10:39:33 +0300

    Готовые крестики-нолики
    Добавлена блокировка ходов человека

--- Крестики-нолики.java
+++ Крестики-нолики.java
@@ -4,20 +4,21 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-  
-    public static boolean flag = false;
+
+    public static boolean flag = false, flagAI = false;
     public static int sumDots = 0;
     public static int SIZE;
     public static int DOTS_TO_WIN;
+    public static int COORD_X, COORD_Y;
     public static final char DOT_EMPTY = '•';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
-    public static char[][] map;
+    public static char[][] map, checkMap;
     public static Scanner in = new Scanner(System.in);
     public static Random rand = new Random();
 
     public static void main(String[] args) {
-        
+
         level();
         initMap();
         printMap();
@@ -53,31 +54,76 @@ public class Main {
         System.out.print("Выберите количество символов для победы: ");
         DOTS_TO_WIN = in.nextInt();
     }
-  
+
+    public static boolean checkForAI(char symb){
+
+        //горизонтали
+        for(int i = 0; i < SIZE; i++){
+            for(int j = 0; j < SIZE; j++){
+                if(map[i][j] == symb) sumDots += 1;
+                else sumDots = 0;
+                if(sumDots == DOTS_TO_WIN - 1) {
+                    flagAI = true;
+                    COORD_X = j + 1;
+                    COORD_Y = i;
+                }
+            }
+        }
+        if(flagAI) return true;
+        sumDots = 0;
+        //вертикали
+        for(int i = 0; i < SIZE; i++){
+            for(int j = 0; j < SIZE; j++){
+                if(map[j][i] == symb) sumDots += 1;
+                else sumDots = 0;
+                if(sumDots == DOTS_TO_WIN - 1) {
+                    flagAI = true;
+                    COORD_X = i;
+                    COORD_Y = j + 1;
+                }
+            }
+        }
+        sumDots = 0;
+
+        return flagAI;
+    }
+
     public static boolean checkWin(char symb) {
 
-        //главная диагональ
-        for(int i = 0; i < SIZE; i++){
-            for(int j = 0; j < SIZE; j++){
-                if(i == j){
-                    if(map[i][j] == symb) sumDots += 1;
-                    else sumDots = 0;
-                    if(sumDots >= DOTS_TO_WIN) flag = true;
+        checkMap = new char[DOTS_TO_WIN][DOTS_TO_WIN];
+
+        for(int n = 0; n < SIZE - DOTS_TO_WIN + 1; n++){
+            for(int m = 0; m < SIZE - DOTS_TO_WIN + 1; m++){
+                //заполнение массива
+                for(int i = 0; i < DOTS_TO_WIN; i++){
+                    for(int j = 0; j < DOTS_TO_WIN; j++){
+                        checkMap[i][j] = map[i + n][j + m];
+                    }
                 }
+                //главная диагональ
+                for(int i = 0; i < DOTS_TO_WIN; i++){
+                    for(int j = 0; j < DOTS_TO_WIN; j++){
+                        if(i == j){
+                            if(checkMap[i][j] == symb) sumDots += 1;
+                            else sumDots = 0;
+                            if(sumDots >= DOTS_TO_WIN) flag = true;
+                        }
+                    }
+                }
+                sumDots = 0;
+                //побочная диагональ
+                for(int i = 0; i < DOTS_TO_WIN; i++){
+                    for(int j = 0; j < DOTS_TO_WIN; j++){
+                        if(i == j){
+                            if(checkMap[i][DOTS_TO_WIN - j - 1] == symb) sumDots += 1;
+                            else sumDots = 0;
+                            if(sumDots >= DOTS_TO_WIN) flag = true;
+                        }
+                    }
+                }
+                sumDots = 0;
             }
         }
-        sumDots = 0;
-        //побочная диагональ
-        for(int i = 0; i < SIZE; i++){
-            for(int j = 0; j < SIZE; j++){
-                if(i == j){
-                    if(map[i][SIZE - j - 1] == symb) sumDots += 1;
-                    else sumDots = 0;
-                    if(sumDots >= DOTS_TO_WIN) flag = true;
-                }
-            }
-        }
-        sumDots = 0;
         //горизонтали
         for(int i = 0; i < SIZE; i++){
             for(int j = 0; j < SIZE; j++){
@@ -109,11 +155,25 @@ public class Main {
 
     public static void aiTurn() {
         int x, y;
-        do {
-            x = rand.nextInt(SIZE);
-            y = rand.nextInt(SIZE);
-        } while (!isCellValid(x, y));
-        System.out.println("Компьютер походил в точку" + (x + 1) + " " + (y + 1));
+
+        if(checkForAI(DOT_X)){
+            x = COORD_X;
+            y = COORD_Y;
+            flagAI = false;
+            if(!isCellValid(x, y)){
+                do {
+                    x = rand.nextInt(SIZE);
+                    y = rand.nextInt(SIZE);
+                } while (!isCellValid(x, y));
+            }
+        }
+        else {
+            do {
+                x = rand.nextInt(SIZE);
+                y = rand.nextInt(SIZE);
+            } while (!isCellValid(x, y));
+        }
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
         map[y][x] = DOT_O;
     }
 
@@ -129,8 +189,7 @@ public class Main {
 
     public static boolean isCellValid(int x, int y) {
         if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-        if (map[y][x] == DOT_EMPTY) return true;
-        return false;
+        return map[y][x] == DOT_EMPTY;
     }
 
     public static void initMap() {
